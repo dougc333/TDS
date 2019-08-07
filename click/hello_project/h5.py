1221956ef2231c43c7764d930e4cee59bf4cbe14
Doug Chang <dougchang@Dougs-MacBook-Pro.local>
2019-08-07 14:01:09 -0700

    add

--- click/hello_project/h5.py
+++ click/hello_project/h5.py
@@ -11,7 +11,7 @@ pass_config = click.make_pass_decorator(Config,ensure=True)
 @pass_config
 def cli(config,verbose):
 	config.verbose = verbose
-	
+	click.echo('cli verbose')	
 
 @cli.command()
 @click.option('--string', default='default string option')
@@ -20,6 +20,7 @@ def cli(config,verbose):
 def foo(config,string,repeat):
 	click.echo('foo string %s' % string)
 	if config.verbose:
-		click.echo('verbose in foo function')
+		click.echo('verbose in foo function where the verbose state \
+		has been transferred to foo from cli using the pass_config decorator')
 	for x in range(repeat):
 		click.echo(x)
