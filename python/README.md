faaa298e53c6841cff4aa6db0b0b984a7142cd04
Doug Chang <dougchang@Dougs-MacBook-Pro.local>
2019-08-20 10:48:05 -0700

    add

--- python/README.md
+++ python/README.md
@@ -1,2 +1,2 @@
 python prog notes for API design
-
+note __init__.py was never called
---
+++ tds_env/bin/startme.py
@@ -0,0 +1,7 @@
+#!/Users/dougchang/TDS/tds_env/bin/python3.7
+# EASY-INSTALL-DEV-SCRIPT: 'startme==0.1','startme.py'
+__requires__ = 'startme==0.1'
+__import__('pkg_resources').require('startme==0.1')
+__file__ = '/Users/dougchang/TDS/python/module_first/startme.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
